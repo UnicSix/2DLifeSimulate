dcf9d513e23cfae483aee62cb66c7b141fbd78dd
Noookia <matchman7980@gmail.com>
2023-10-01 19:16:29 +0800

    spawn and kill by clicking

--- Scripts/area_test.gd
+++ Scripts/area_test.gd
@@ -1,11 +1,41 @@
-extends CharacterBody2D
+extends Area2D
 
+var botInArea
+var pub_col
+var rcv_col
 func _ready():
-	pass
+	botInArea = 0 
+	pub_col = self.get_parent().get_node('pub/col1')
+	rcv_col = self.get_parent().get_node('rcv/col2')
+
 
 func _process(_delta):
-	pass
+	self.get_parent().get_node('botNum').text = str(botInArea)
 
 func _draw():
-	draw_circle(Vector2(0,0), $Area2D/CollisionShape2D.shape.radius,
+	draw_circle(Vector2(0,0), pub_col.shape.radius,
 		Color(1,1,1,0.2))
+	draw_circle(Vector2(0,0), rcv_col.shape.radius,
+		Color(0,0,0,0.2))
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("left click"):
+		pass
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+
+
+func _on_area_exited(_area:Area2D):
+	botInArea-=1
+
+func _on_area_entered(_area:Area2D):
+	botInArea+=1
+
+
+func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int):
+	if Input.is_action_just_pressed('left click'):
+		pub_col.set_deferred('disabled', true)
+	elif Input.is_action_just_pressed('right click'):
+		pub_col.set_deferred('disabled', false)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 var isAlive
 var botInArea
 var rcv_click = false
@@ -16,21 +15,10 @@ func _ready():
 func _process(_delta):
 	pass
 		
-func _on_area_entered(_area):
-	botInArea+=1
 	
 func _unhandled_input(_event):
 	if Input.is_action_pressed("start"):
 		$Timer.start()
-	if Input.is_action_just_pressed("right click") and rcv_click:
-		pub_col.set_deferred('disabled', true)
-		py_sprite.set_deferred('visible', false)
-		botInArea-=1
-	elif Input.is_action_just_pressed("left click") and rcv_click:
-		print(botInArea)
-		pub_col.set_deferred('disabled', false)
-		py_sprite.set_deferred('visible', true)
-		
 	
 func _on_timer_timeout():
 	isAlive = not self.get_parent().get_node('pub/pub_col1').disabled
@@ -50,15 +38,27 @@ func _on_timer_timeout():
 		py_sprite.set_deferred('visible', false)
 		botInArea-=1
 
+func _on_area_entered(_area):
+	botInArea+=1
+
 func _on_area_exited(_area):
 	botInArea-=1
-	
 
 func _on_mouse_entered():
 	rcv_click = true
-	# print("enter")
-
 
 func _on_mouse_exited():
 	rcv_click = false
-	# print("exit")
+
+# func _draw():
+# 	var col_rec = Rect2(pub_col.get_parent().position, pub_col.shape.size)
+# 	draw_rect(col_rec, Color(1,1,1,0.2), false, 1)
+
+func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int):
+	if Input.is_action_just_pressed("right click"):
+		pub_col.set_deferred('disabled', true)
+		py_sprite.set_deferred('visible', false)
+	elif Input.is_action_just_pressed("left click"):
+		print(botInArea)
+		pub_col.set_deferred('disabled', false)
+		py_sprite.set_deferred('visible', true)
